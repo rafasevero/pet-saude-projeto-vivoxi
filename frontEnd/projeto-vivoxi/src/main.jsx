a0f969ef455df44092945c8843a59633f23e796d
Luan <luanpck@hotmail.com>
2026-02-26 23:45:08 -0300

    fix: update Vite plugin to use @vitejs/plugin-react and clean up imports

--- frontEnd/projeto-vivoxi/src/main.jsx
+++ frontEnd/projeto-vivoxi/src/main.jsx
@@ -1,12 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Home from './pages/Home'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Login from './pages/Login/login.jsx'
+import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage/landingPage.jsx'
+import Login from './pages/Login/login.jsx'
+import Home from './pages/Home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
